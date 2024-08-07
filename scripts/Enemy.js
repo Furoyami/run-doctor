63fbc5d04d0282d475c8d9a48ce14b5199c5918b
Furo <florian.a.demette@gmail.com>
2024-08-07 15:37:44 +0200

    clean

--- scripts/Enemy.js
+++ scripts/Enemy.js
@@ -3,8 +3,6 @@ class Enemy {
         let imgEnemy = imageLoader.getImage("images/dalek_tile.png");
         this.spriteEnemy = new Sprite(imgEnemy);
         this.spriteEnemy.setTileSheet(40, 41);
-        this.spriteEnemy.line = pLine;
-        this.spriteEnemy.col = pCol;
         this.spriteEnemy.x = pCol * myGrid.cellSize;
         this.spriteEnemy.y = pLine * myGrid.cellSize;
         this.spriteEnemy.vX = 0;
@@ -18,25 +16,10 @@ class Enemy {
     }
 
     Update(dt, lstEnemies) {
-        if (this.spriteEnemy === null) {
-            if (debug) console.log("spriteEnemy is null in Update function");
-        }
-
         //----- Récupération de la position du joueur -----
         let playerPos = player.getPlayerPos();
         let playerY = playerPos[0] * myGrid.cellSize;
         let playerX = playerPos[1] * myGrid.cellSize;
 
-        let nbLines = myGrid.getGridNbLines();
-        let nbCols = myGrid.getGridNbColumns();
-
-        let enemyLine = this.spriteEnemy.line;
-        let enemyCol = this.spriteEnemy.col;
-
-        if (enemyLine >= 0 && enemyLine < nbLines
-            && enemyCol >= 0 && enemyCol < nbCols
-        ) {
-
-        }
     }
 }
